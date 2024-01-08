5ebbf31e7055ccef3a8cc16518be9e21eddf8b9f
LorinHan <627075866@qq.com>
2024-01-08 09:43:16 +0800

    fix: 调整cache包的api

--- template/kit/cache/redis_aop.go
+++ template/kit/cache/redis_aop.go
@@ -20,7 +20,7 @@ type KeyFunc func() (key string, data interface{}, err error)
 type KeyDelFunc func() (key string, err error)
 type DelFunc func() error
 
-func get(ctx context.Context, key string, v interface{}) (bool, error) {
+func Get(ctx context.Context, key string, v interface{}) (bool, error) {
 	data, err := rdb.Get(ctx, key).Result()
 	if err != nil {
 		if err == redis.Nil {
@@ -36,7 +36,23 @@ func get(ctx context.Context, key string, v interface{}) (bool, error) {
 	return true, nil
 }
 
-func set(ctx context.Context, key string, data, container interface{}, expiration ...time.Duration) error {
+func Set(ctx context.Context, key string, data interface{}, expiration ...time.Duration) error {
+	if data == nil {
+		return nil
+	}
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	var ex time.Duration
+	if len(expiration) > 0 {
+		ex = expiration[0]
+	}
+	return rdb.Set(ctx, key, jsonData, ex).Err()
+}
+
+func setAndUnmarshal(ctx context.Context, key string, data, container interface{}, expiration ...time.Duration) error {
 	if data == nil {
 		return nil
 	}
@@ -61,7 +77,7 @@ func set(ctx context.Context, key string, data, container interface{}, expiratio
 
 // Cacheable 执行回调函数前会查询缓存，若key不存在则执行回调，将回调执行结果放入缓存，若key存在，将数据映射到参数v(应传入指针)且不执行回调函数
 func Cacheable(ctx context.Context, key string, v interface{}, f Func, expiration ...time.Duration) error {
-	exist, err := get(ctx, key, v)
+	exist, err := Get(ctx, key, v)
 	if err != nil {
 		return err
 	}
@@ -74,7 +90,7 @@ func Cacheable(ctx context.Context, key string, v interface{}, f Func, expiratio
 		return err
 	}
 
-	return set(ctx, key, res, v, expiration...)
+	return setAndUnmarshal(ctx, key, res, v, expiration...)
 }
 
 // Evict 执行回调函数后，删除该key的缓存
@@ -103,7 +119,7 @@ func Put(ctx context.Context, key string, f Func, expiration ...time.Duration) e
 		return err
 	}
 
-	return set(ctx, key, res, nil, expiration...)
+	return Set(ctx, key, res, expiration...)
 }
 
 // PutByDynamicKey 执行回调函数后，将回调执行结果放入缓存，key取决于回调函数的返回值，与Cacheable不同的是PutByDynamicKey不会进行前置查询，常用于更新操作
@@ -113,5 +129,5 @@ func PutByDynamicKey(ctx context.Context, f KeyFunc, expiration ...time.Duration
 		return err
 	}
 
-	return set(ctx, key, res, nil, expiration...)
+	return Set(ctx, key, res, expiration...)
 }
